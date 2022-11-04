95139b4cd894adccbcda8a04d26d37176111798d
aror raximmov <axrorraximov108@gmail.com>
2022-11-04 17:14:47 +0500

    02

--- bin/map_methods02.dart
+++ bin/map_methods02.dart
@@ -3,12 +3,12 @@ Car details will be given to you in map view, change the car price for it
 */
 
 void main() {
-  print(func({'name': 'Mustan', 'color': 'silver', 'price': 50000}, 80000));
+  print(func({'name': 'Mustan', 'color': 'silver', 'price': 5}, 8));
 }
 
 Map func(Map data, int cost) {
   //your code here
-  data['price'] = 80000;
+  data['price'] = 8;
 
   return data;
 }
