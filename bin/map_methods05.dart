11a60f61edf0780d85d5b10c328caeb124abb324
aror raximmov <axrorraximov108@gmail.com>
2022-11-04 18:01:17 +0500

    05

--- bin/map_methods05.dart
+++ bin/map_methods05.dart
@@ -4,10 +4,16 @@ the key in it is last name,
 change it to surname
 */
 
-void main() {}
+void main() {
+  Map mp = {'ismim': 'axror', 'Familyam': 'Raximov'};
+  print(func(mp));
+}
 
 Map func(Map data) {
+  String a = data['Familyam'];
+  data.remove('Familyam');
   //your code here
+  data['surname'] = a;
 
   return data;
 }
